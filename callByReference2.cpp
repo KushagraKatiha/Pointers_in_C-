811073534df79a181490a3a381dae6b11e450ef3
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-01-20 16:39:35 +0530

    call by ref using &

--- callByReference2.cpp
+++ callByReference2.cpp
@@ -18,6 +18,23 @@ void getIndecies(string str, char ch, int *ptr1, int *ptr2){
     }
 }
 
+// Function to get first and last indecis of ch in str using address of operater
+void getIndecies(string str, char ch, int &first, int &last){
+    for(int i = 0; i < str.size(); i++){
+        if(str[i] == ch){
+            first = i;
+            break;
+        }
+    }
+
+    for(int i = str.size(); i >= 0; i--){
+        if(str[i] == ch){
+            last = i;
+            break;
+        }
+    }
+}
+
 int main(){
     // Find first and last occurence of given character in given string
     string str = "aabcad";
@@ -29,7 +46,8 @@ int main(){
     int *firstIdxPtr = &firstIdx;
     int *lastIdxPtr = &lastIdx;
 
-    getIndecies(str, ch, firstIdxPtr, lastIdxPtr);
+    // getIndecies(str, ch, firstIdxPtr, lastIdxPtr);
+    getIndecies(str, ch, firstIdx, lastIdx);
 
     cout<<*firstIdxPtr<<" "<<*lastIdxPtr<<endl;
     cout<<firstIdx<<" "<<lastIdx<<endl;
