0ea044d76150cb44847c267a082f386a0f7bba1b
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-01-20 16:12:37 +0530

    call by reference 1

---
+++ callByReference.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+// Function to swap the values of the x and y
+void swap(int x, int y){
+    
+    // Here, call by value is used therefore there will be no change in the original values
+    int temp = x;
+    x = y;
+    y = temp;
+
+}
+
+// To achieve this we can use call by reference using pointers
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main(){
+
+    int x = 10;
+    int y = 3;
+
+    int *ptrx = &x;
+    int *ptry = &y;
+
+    swap(x, y);
+    cout<<"x: "<<x<<" y: "<<y<<endl;
+
+    // swap on passing pointers
+    swap(ptrx, ptry);
+    cout<<"x: "<<x<<" y: "<<y<<endl;
+    return 0;
+}
